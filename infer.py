bc96c83bb7ac27d74f35c0c5c7aed2ca2ea6bf18
Ethcad <brendon-m@outlook.com>
2017-07-08 11:34:48 -0400

    First workable testing version

--- infer.py
+++ infer.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python2
 
 import numpy as np
+import os
 
 from keras.models import load_model
 from skimage.io import imread
 from sys import argv
 
 model = load_model("./%s" % argv[1])
+os.system("rm /tmp/sim* && echo 0.0 > /tmp/sim")
 
-for path in argv[2:]:
-    image_raw = imread(path).astype(np.float32)
-    image_3d = np.transpose(image_raw, (1, 0, 2))
-    image = np.expand_dims(image_3d, 0)
-    steering_angle = model.predict(image)
-    print(steering_angle)
+i = 1
+while True:
+    if os.path.isfile("/tmp/sim%d.png" % i):
+        image_raw = imread("/tmp/sim%d.png" % (i - 1)).astype(np.float32)
+        image_3d = np.transpose(image_raw, (1, 0, 2))
+        image = np.expand_dims(image_3d, 0)
+        steering_angle = model.predict(image)
+        os.system("echo %f > /tmp/sim" % steering_angle)
+        i += 1
