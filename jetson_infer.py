3018b7c31f73b371670f001c1eae54d1b3eeb796
Ethcad <brendon-m@outlook.com>
2017-08-07 10:59:38 -0400

    Implemented Jetson inference script

--- jetson_infer.py
+++ jetson_infer.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python2
 
+import tensorflow as tf
 import numpy as np
 import os
 
+from keras.backend.tensorflow_backend import set_session
 from keras.models import load_model
 from skimage.io import imread
+from scipy.misc import imresize
 from sys import argv
 
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.3
+set_session(tf.Session(config=config))
+
+model = load_model("./%s" % argv[1])
 model = load_model("./%s" % argv[1])
 fs_path = "/tmp/"
 os.system("rm %s*sim*" % fs_path)
@@ -14,10 +22,11 @@ os.system("echo 0.0 > %s-1sim.txt" % fs_path)
 
 i = 0
 while True:
-    path = "%ssim%d.png" % (fs_path, i)
+    path = "%ssim%d.jpg" % (fs_path, i)
     if os.path.isfile(path):
         image_raw = imread(path).astype(np.float32)
-        image_3d = np.transpose(image_raw, (1, 0, 2))[:, 66:132, :]
+        image_3d = np.transpose(image_raw, (1, 0, 2))[240:1680, :, :]
+        image_small = imresize(image_3d, (200, 150, 3), interp='lanczos')[:, 66:132, :]
         image = np.expand_dims(image_3d, 0)
         steering_angle = model.predict(image)
         os.system("echo %f > %stemp.txt" % (steering_angle, fs_path))
--- webcam_stream.sh
+++ webcam_stream.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 gst-launch-1.0 -v v4l2src device=/dev/video1 \
 ! image/jpeg, width=1920, height=1080, framerate=30/1 \
-! jpegparse ! multifilesink location="frame%d.jpg"
+! jpegparse ! multifilesink location="sim%d.jpg"
